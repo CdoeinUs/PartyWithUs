b72c6a2b2d90b110799281592ebde23bd6eab84d
codekunst <alstjrwjd99@naver.com>
2023-04-06 18:55:32 +0900

    바텀바, myhome

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:partywithus/recipe_search.dart';
+import 'liquor_search.dart';
 import 'main_second.dart';
 import 'model.dart';
 import 'cocktail_Detail.dart';
+import 'mypage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -46,110 +49,200 @@ class _CocktailsState extends State<Cocktails> {
   ];
   static List<String> cocktailBase = ['와인', '진', '럼', '보드카'];
 
+  int _currentIndex = 0;
+
+  void _onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return _getPage(index);
+    }));
+  }
+
   final List<Cocktail> cocktailData = List.generate(
       cocktailName.length,
-      (index) => Cocktail(cocktailName[index], cocktailImagePath[index],
-          cocktailDetail[index], cocktailBase[index]));
+          (index) =>
+          Cocktail(cocktailName[index], cocktailImagePath[index],
+              cocktailDetail[index], cocktailBase[index]));
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(children: [
-      Container(
-          padding: const EdgeInsets.all(20),
-          width: MediaQuery.of(context).size.width,
-          height: (MediaQuery.of(context).size.height) * 0.5,
-          color: Colors.black,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox.fromSize(
-                size: Size.fromHeight(
-                    (MediaQuery.of(context).size.height) * 0.008),
-              ),
-              const Text("OUNCE\n",
-                  style: TextStyle(color: Colors.white, fontSize: 28)),
-              SizedBox.fromSize(
-                size: Size.fromHeight(
-                    (MediaQuery.of(context).size.height) * 0.11),
-              ),
-              const Text("퇴근 후\n완벽한 밤을 위한\nSEONGSU JAZZ BAR",
-                  style: TextStyle(color: Colors.white, fontSize: 28)),
-              SizedBox.fromSize(
-                size: Size.fromHeight(
-                    (MediaQuery.of(context).size.height) * 0.03),
-              ),
-              const Text("200여종의 칵테일을 경험해 보세요",
-                  style: TextStyle(color: Colors.white, fontSize: 13)),
-            ],
-          )),
-      Container(
-        padding: const EdgeInsets.fromLTRB(20, 19, 0, 0),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.12,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: const [
-            Text(
-              "당신에게 딱\n맞춰진 칵테일을 찾아보세요!",
-              style: TextStyle(
+      body: Center(
+          child: Column(children: [
+            Container(
+                padding: const EdgeInsets.fromLTRB(20,20,20,0),
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                height: (MediaQuery
+                    .of(context)
+                    .size
+                    .height) * 0.4,
                 color: Colors.black,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox.fromSize(
+                      size: Size.fromHeight(
+                          (MediaQuery
+                              .of(context)
+                              .size
+                              .height) * 0.003),
+                    ),
+                    const Text("OUNCE\n",
+                        style: TextStyle(color: Colors.white, fontSize: 25)),
+                    SizedBox.fromSize(
+                      size: Size.fromHeight(
+                          (MediaQuery
+                              .of(context)
+                              .size
+                              .height) * 0.05),
+                    ),
+                    const Text("퇴근 후\n완벽한 밤을 위한\nSEONGSU JAZZ BAR",
+                        style: TextStyle(color: Colors.white, fontSize: 25)),
+                    SizedBox.fromSize(
+                      size: Size.fromHeight(
+                          (MediaQuery
+                              .of(context)
+                              .size
+                              .height) * 0.03),
+                    ),
+                    const Text("200여종의 칵테일을 경험해 보세요",
+                        style: TextStyle(color: Colors.white, fontSize: 13)),
+                  ],
+                )),
+            Container(
+              padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              height: MediaQuery
+                  .of(context)
+                  .size
+                  .height * 0.12,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "당신에게 딱\n맞춰진 칵테일을 찾아보세요!",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
-      Container(
-        alignment: Alignment.center,
-        width: MediaQuery.of(context).size.width,
-        height: (MediaQuery.of(context).size.height) * (0.3),
-        child: ListView.builder(
-            itemCount: cocktailData.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) =>
-                            CocktailPage(cocktail: cocktailData[index])));
-                  },
-                  child: Column(
-                    children: [
-                      Image.asset(cocktailData[index].imgPath),
-                      Text(cocktailData[index].name),
-                    ],
-                  ),
-                ),
-              );
-            }),
-      ),
-      Container(
-          child: ElevatedButton(
-              child: Text(
-                "더 보고 싶어요",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold),
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey[200], // 버튼 색상
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20.0), // 버튼 모서리 둥글기
-                ),
-                padding: EdgeInsets.symmetric(
-                    horizontal: 20.0, vertical: 10.0), // 버튼 패딩
+            Container(
+              alignment: Alignment.center,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width,
+              height: (MediaQuery
+                  .of(context)
+                  .size
+                  .height) * (0.15),
+              child: ListView.builder(
+                  itemCount: cocktailData.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) =>
+                                  CocktailPage(cocktail: cocktailData[index])));
+                        },
+                        child: Column(
+                          children: [
+                            Image.asset(cocktailData[index].imgPath),
+                            Text(cocktailData[index].name),
+                          ],
+                        ),
+                      ),
+                    );
+                  }),
+            ),
+            Container(
+                child: ElevatedButton(
+                    child: Text(
+                      "더 보고 싶어요",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey[200], // 버튼 색상
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0), // 버튼 모서리 둥글기
+                      ),
+                      padding: EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0), // 버튼 패딩
 
-              ),
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context2) => const SecondPage()));
-              })),
-    ])));
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context2) => const SecondPage()));
+                    })),
+            _getPage(_currentIndex),
+          ])),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.black,        // 탭 항목 목록
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.article),
+            label: 'community',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'main',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'my account',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'alarm',
+          ),
+        ],
+        currentIndex: _currentIndex, // 현재 선택된 탭 인덱스
+        onTap: _onTabTapped, // 탭 선택 이벤트 핸들러
+      ),
+    );
   }
 }
+Widget _getPage(int currentIndex) {
+  switch (currentIndex) {
+    case 0:
+      return liquorSearch();
+      //커뮤니티 페이지로 바껴야함
+    case 1:
+      return RecipeSearch();
+    case 2:
+      return Cocktails();
+    case 3:
+      return myHome();
+      //account로 바껴야함
+    case 4:
+      return SecondPage();
+      //알림 페이지로 바껴야함
+    default:
+      return Cocktails();
+  }
+}
+
---
+++ lib/mypage.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:partywithus/model.dart';
+
+class myHome extends StatefulWidget {
+  const myHome({Key? key}) : super(key: key);
+
+  @override
+  State<myHome> createState() => _myHomeState();
+}
+
+class _myHomeState extends State<myHome> {
+
+  static List<String> cocktailName = ['뱅쇼', '하이볼', '블루라군', '동해'];
+  static List<String> cocktailImagePath = [
+    'image/cocktail1.png',
+    'image/cocktail2.png',
+    'image/cocktail3.png',
+    'image/cocktail4.png',
+  ];
+  static List<String> cocktailDetail = [
+    '뱅쇼(vin chaud)는 프랑스어로 "따뜻한 와인"이란 뜻으로, 와인에 여러 과일과 계피를 비롯한 향신료를 넣고 끓여 만든 음료수를 말한다.',
+    '칵테일 제조법의 일종. 기본은 얼음을 채운 텀블러 글라스에 증류주를 일정량 넣고 그 위에 탄산수, 진저에일 등의 탄산음료를 부어 만든다. 증류주로는 위스키가 제일 흔히 쓰인다. 여기에 레몬 혹은 라임 등을 필하여 상큼한 맛을 더하는 것도 가능하다.',
+    '보드카 베이스 칵테일로, 이름의 뜻은 푸른 산호초. 그래서인지 칵테일 색도 마찬가지로 푸른색이다. 두 가지 버전이 있는데, 하나는 구프 글라스에 따르는 숏 드링크, 하나는 하이볼 글라스 또는 콜린스 글라스에 따르는 롱 드링크다. 때문에 바에서 블루 라군을 주문할 때는 꼭 정확한 표현을 사용해서 주문해야 된다.',
+    ' 칵테일 동해를 비롯한 시중에서 잘 나가는 종류의 칵테일들은 공통점이라면 특유의 개성적인 맛이 있다기보단 색상적인 화려함과 이미지, 그리고 누가 마셔도 맛있게 느낄만한 달콤함과 새콤함 등 무난한 맛이 특징이라 할 수 있습니다. 그렇기에 거의 공통적으로 쓰이는 재료를 들자면 복숭아 리큐르인 피치 시냅스, 즉 피치 트리, 코코넛 럼인 말리부 또는 그와 유사한 상품, 푸른색을 내는 대표적인 재료인 블루 큐라소, 마지막으로 스위트&사워 믹스(Sweet&Sour Mix) 줄여서 사워 믹스라 부르는 것들이 있습니다.'
+  ];
+  static List<String> cocktailBase = ['와인', '진', '럼', '보드카'];
+
+  final List<Cocktail> cocktailData = List.generate(
+      cocktailName.length,
+          (index) =>
+          Cocktail(cocktailName[index], cocktailImagePath[index],
+              cocktailDetail[index], cocktailBase[index]));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(30, 30, 10, 0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start, // 세로 방향으로 왼쪽 정렬
+          children: [
+            Row(
+              children: [
+                Text("OUNCE",
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    )),
+                Spacer(),
+                Icon(Icons.account_circle)
+              ],
+            ),
+            Divider(
+              color: Colors.black,
+              height: 10,
+              thickness: 3,
+              indent: 0,
+              endIndent: 8,
+            ),
+            SizedBox(height: 20,),
+
+            const Text(
+              "카테고리",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+
+            Container(
+              height : (MediaQuery.of(context).size.height) * 0.3,
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 20),
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  Row(children: [
+                    Icon(Icons.favorite),
+                    Text("좋아요 표시한 술"),
+                  ]),
+                  SizedBox(height: 20,),
+                  Row(children: [
+                    Icon(Icons.article),
+                    Text("내가 쓴 글"),
+                  ]),
+                  SizedBox(height: 20,),
+                  Row(children: [
+                    Icon(Icons.message),
+                    Text("댓글 단 글"),
+                  ]),
+                  SizedBox(height: 20,),
+                  Row(children: [
+                    Icon(Icons.local_bar),
+                    Text("먹고 있는 술"),
+                  ]),
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                OutlinedButton(
+                  onPressed: () {
+                    // 버튼이 눌렸을 때 실행될 코드
+                  },
+                  child: Text("베이스"),
+                ),
+                SizedBox(width: 10,),
+                OutlinedButton(
+                  onPressed: () {
+                    // 버튼이 눌렸을 때 실행될 코드
+                  },
+                  child: Text("리큐르"),
+                ),
+                SizedBox(width: 10,),
+                OutlinedButton(
+                  onPressed: () {
+                    // 버튼이 눌렸을 때 실행될 코드
+                  },
+                  child: Text("가니쉬"),
+                ),
+                SizedBox(width: 10,),
+                OutlinedButton(
+                  onPressed: () {
+                    // 버튼이 눌렸을 때 실행될 코드
+                  },
+                  child: Text("기타"),
+                )
+              ],
+            ),
+            Container(
+              height : (MediaQuery.of(context).size.height) * 0.5,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemCount: cocktailName.length, // 리스트의 아이템 개수
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                      child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(cocktailImagePath[index]),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          child: Text(cocktailName[index],style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white
+                          ),)// Card의 내용을 추가합니다
+                      ),
+                    );
+                  },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
